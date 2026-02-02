8fc625accfe80fcf2327ff17dc9cc177536acbaf
Renzo <krcondori@gmail.com>
2026-02-01 22:53:58 -0500

    feat: initial project with menu

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,43 @@
+package principal;
+
+import java.util.Scanner;
+
+
+public class Principal {
+    private final Scanner teclado = new Scanner(System.in);
+
+    public void mostrarMenu(){
+        var opcion = -1;
+        while (opcion != 0){
+            var menu = """
+                    -------------------------------
+                            Menu Principal
+                    -------------------------------
+                     1 - Buscar Libro por titulo
+                     2 - Listar libros registrados
+                     3 - Listar autores registrados
+                     4 - Listar autores vivos por X año
+                     5 - Listar libros por idioma
+                     6 - Top 10 libros con más descargas
+                     7 - Algunas estadísticas
+                    
+                     0 - Salir
+                    """;
+            System.out.println(menu);
+            System.out.print("Elija una opcion: ");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("caso 1 ingresando");
+                case 0:
+                    System.out.println("Cerrando la aplicacion");
+                default:
+                    System.out.println("Opcion invalida");
+            }
+        }
+
+
+    }
+}
